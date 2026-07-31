4feae423a2649d0056a84ebaf44bab689ed02f95
MaximAbramov <maximab2002@gmail.com>
2026-07-31 19:51:36 +0300

    Small patch new sprites
    Added new sprite
    New floor One sprite
    New floor Two sprite
    New "Bullet" HollyWater

--- abel/obj/Debug/net9.0/Abel The Last Son.AssemblyInfo.cs
+++ abel/obj/Debug/net9.0/Abel The Last Son.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Abel The Last Son")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd1458c221bd2e69583d3dd75f7179d93bb8565a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Abel The Last Son")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Abel The Last Son")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
